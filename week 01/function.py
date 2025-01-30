119b06e3d78d57b169fda7f6e682193f65a758ec
Syed Abtahi Bin Mashak <syedabtahibinmashak@gmail.com>
2025-01-31 04:51:44 +0600

    ===

---
+++ week 01/function.py
@@ -0,0 +1,10 @@
+def print_nid(name,dob,gender,blood,father,mother,idno):
+    print(f"Name         : {name}")
+    print(f"Date of Birth: {dob}")
+    print(f"Gender       : {gender}")
+    print(f"Blood Group  : {blood}")
+    print(f"Father's Name: {father}")
+    print(f"Mother's Name: {mother}")
+    print(f"NID No       : {idno}")
+
+print_nid("SpongeBob SquarePants","14-07-1986","Male","O +ve","Harold SquarePants","Margaret SquarePants","230041135")
