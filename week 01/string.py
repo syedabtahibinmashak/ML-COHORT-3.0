31733a1edbdc1d18e1eacf7a963e6d1069a7c962
Syed Abtahi Bin Mashak <syedabtahibinmashak@gmail.com>
2025-01-31 03:50:06 +0600

    ===

---
+++ week 01/string.py
@@ -0,0 +1,19 @@
+# problem 01
+x = "ABRACADABRA"
+print(x[2])
+
+# problem 02
+x = "The quick brown fox jumps over the lazy dog"
+print(x[10:19])
+
+# problem 03
+A = "বাংলা"
+B = "আমার"
+C = "সোনার"
+text = B + " " + C + " " + A
+print(text)
+
+# problem 04
+singer_name = "Anupom Roy"
+song_name = "Benche Thakar Gaan"
+print(f"My favourite singer is {singer_name}, whose most popular song is {song_name}")
